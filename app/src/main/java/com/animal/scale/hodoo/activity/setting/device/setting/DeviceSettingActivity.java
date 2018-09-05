f972f6617757012ca7b07c02556e3b9e5eb604ed
silverlight2017@gmail.com <1213dlgudwns>
2018-09-05 18:12:13 +0900

    created

---
+++ app/src/main/java/com/animal/scale/hodoo/activity/setting/device/setting/DeviceSettingActivity.java
@@ -0,0 +1,15 @@
+package com.animal.scale.hodoo.activity.setting.device.setting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.animal.scale.hodoo.R;
+
+public class DeviceSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_setting);
+    }
+}
